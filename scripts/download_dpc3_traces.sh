b49e97dbc67ea0aaa7e3ea46f4c123f1a8d81913
Daniel Suo <danielsuo@gmail.com>
2019-09-12 23:21:42 -0400

    Update download dpc3 script

--- scripts/download_dpc3_traces.sh
+++ scripts/download_dpc3_traces.sh
@@ -4,4 +4,4 @@ mkdir -p $PWD/../dpc3_traces
 while read LINE
 do
     wget -P $PWD/../dpc3_traces -c http://hpca23.cse.tamu.edu/champsim-traces/speccpu/$LINE
-done < dpc3_max_simpoint.txt
+done < scripts/dpc3_max_simpoint.txt
